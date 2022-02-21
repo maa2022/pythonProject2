a831e806bad562124f4a8aea975b6ec87fdd37e5
maa2022 <99119977+maa2022@users.noreply.github.com>
2022-02-21 23:34:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
+#импорт модулей
 from tkinter import *
 import time
 import random
 
+#создаем переменную со значением истина
 Game_Running = True
 
+#размеры игровой области
 game_width = 400
 game_height = 400
+#размер 1 ячейки
 snake_item = 20
+
+#цвет змейки
 snake_color1 = "black"
 snake_color2 = "yellow"
 
+#создаем переменные для виртуальных координат
 virtual_game_x = game_width // snake_item
 virtual_game_y = game_height // snake_item
 
+#первый элемент змейки по координатам
 snake_x = random.randrange(virtual_game_x )
 snake_y = random.randrange(virtual_game_y )
+#направление движения змейки
 snake_x_nav = 0
 snake_y_nav = 0
 
+#список для координат элементов змейки
 snake_list = []
+#размер змейки
 snake_size = 5
 
+#создание окна
 tk = Tk()
 tk.title("Snake")
 tk.resizable(0, 0)
+
+#атрибут - окно поверх остальных
 tk.wm_attributes("-topmost", 1)
+
+#создание объекта(игровое поле)
 canvas = Canvas(tk, width=game_width, height=game_height, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
+
+#цвет еды
 present_color1 = "red"
 present_color2 = "black"
+#список для хранения "еды"
 presents_list = []
+#количество
 presents_size = 5
+#заполняем список координатами
 for i in range(presents_size):
     x = random.randrange(virtual_game_x)
     y = random.randrange(virtual_game_y)
@@ -44,6 +65,7 @@ for i in range(presents_size):
 print(presents_list)
 
 
+#функция отрисовывания змейки
 def snake_paint_item(canvas, x, y):
     global snake_list
     id1 = canvas.create_rectangle(x * snake_item, y * snake_item, x * snake_item + snake_item,
@@ -53,10 +75,10 @@ def snake_paint_item(canvas, x, y):
     snake_list.append([x, y, id1, id2])
     # print(snake_list)
 
-
 snake_paint_item(canvas, snake_x, snake_y)
 
 
+#проверяем, что не превышен размер змеи
 def check_can_we_delete_snake_item():
     if len(snake_list) >= snake_size:
         temp_item = snake_list.pop(0)
@@ -65,6 +87,7 @@ def check_can_we_delete_snake_item():
         canvas.delete(temp_item[3])
 
 
+#проверяем коррдинаты змеи на наличие в списке с едой
 def check_if_we_found_present():
     global snake_size
     for i in range(len(presents_list)):
@@ -75,6 +98,7 @@ def check_if_we_found_present():
             canvas.delete(presents_list[i][3])
 
 
+#функция отвечает за принятие событый
 def snake_move(event):
     global snake_x
     global snake_y
@@ -103,22 +127,26 @@ def snake_move(event):
     check_if_we_found_present()
 
 
+#обработка событий
 canvas.bind_all("<KeyPress-Left>", snake_move)
 canvas.bind_all("<KeyPress-Right>", snake_move)
 canvas.bind_all("<KeyPress-Up>", snake_move)
 canvas.bind_all("<KeyPress-Down>", snake_move)
 
 
+#функция прерывания игры
 def game_over():
     global Game_Running
     Game_Running = False
 
 
+#проверка выхода за пределы экрана
 def check_if_borders():
     if snake_x > virtual_game_x or snake_x < 0 or snake_y > virtual_game_y or snake_y < 0:
         game_over()
 
 
+#проверка касанияли змейки самой себя
 def check_we_touch_self(f_x, f_y):
     global Game_Running
     if not (snake_x_nav == 0 and snake_y_nav == 0):
@@ -128,6 +156,7 @@ def check_we_touch_self(f_x, f_y):
                 Game_Running = False
 
 
+#создаем цикл для постоянного движения змейки
 while Game_Running:
     check_can_we_delete_snake_item()
     check_if_we_found_present()
@@ -138,13 +167,13 @@ while Game_Running:
     snake_paint_item(canvas, snake_x, snake_y)
     tk.update_idletasks()
     tk.update()
+    #задержка
     time.sleep(1)
 
 
+#прерывание обработки событий при достижении конца игры
 def fun_nothing(event):
     pass
-
-
 canvas.bind_all("<KeyPress-Left>", fun_nothing)
 canvas.bind_all("<KeyPress-Right>", fun_nothing)
 canvas.bind_all("<KeyPress-Up>", fun_nothing)
